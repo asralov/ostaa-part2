7b7aac8d85fb0d71dac30827043e7abc46568035
Abrorjon Asralov <asralov@arizona.edu>
2023-11-01 13:59:48 -0700

    did some changes on the login page
    Almost finished making changes in the login page so people can easily switch between log in and create account promts, and it is the same page in order to keep the structure for the class

--- public_html/index.js
+++ public_html/index.js
@@ -1,27 +1,20 @@
+
+isLoginPage = true;
+
 // need to have a function to return a create account
 const box = document.getElementById("mainBox");
-
+const btn = document.getElementById("userLogButtonBox");
+const title = document.getElementById("logTitle");
+const promt = document.getElementById("promt");
 function getCreateAccPage(){
-    box.innerHTML = `<div id="title">
-    <h1>Ostaa</h1>
-</div>
-<div id="formBox">
-    <div id="loginOrCreateTitle">
-        <h3>Log In</h3>
-        <div id="userBox">
-            <div id="userName">
-                <span>Username</span>
-                <input type="text" id="user">
-            </div>
-            <div id="userPassword">
-                <span>Password</span>
-                <input type="password" id="password">
-            </div>
-            <div id="userAddButtonBox">
-                <button onclick="addUser();" id="userAddButton">
-                    Add User
-                </button>
-            </div>
-        </div> `
+    btn.innerHTML = `<button onclick="addUser();" id="userAddButton">Create Account</button>`;
+    title.innerHTML = `Create Account`
+    promt.innerHTML = `<span id="promt">Already got an account?<button onclick="getCreateLogPage()" id="createAcc">Log in</button></span>`;
+    
 }
 
+function getCreateLogPage(){
+    btn.innerHTML = `<button onclick="loginUser();" id="userLogButton">Log in</button>`;
+    title.innerHTML = `Log In`
+    promt.innerHTML = `<span id="promt">Do not have an account?<button onclick="getCreateAccPage()" id="createAcc">Sign Up</button></span>`;
+}
--- server.js
+++ server.js
@@ -3,4 +3,5 @@ const app = express();
 const port = 80;
 
 app.use(express.static('public_html'));
+// this is to listen to the server
 app.listen(port, ()=>{console.log("Success!")});
