b5d903811eec5922d7f212f6da9655d859237e5e
Abrorjon Asralov <asralov@arizona.edu>
2023-11-01 15:29:30 -0700

    finished the login page
    finished the login page

--- public_html/index.js
+++ public_html/index.js
@@ -10,11 +10,16 @@ function getCreateAccPage(){
     btn.innerHTML = `<button onclick="addUser();" id="userAddButton">Create Account</button>`;
     title.innerHTML = `Create Account`
     promt.innerHTML = `<span id="promt">Already got an account?<button onclick="getCreateLogPage()" id="createAcc">Log in</button></span>`;
+    document.getElementById("user").value = "";
+    document.getElementById("password").value = "";
+
     
 }
 
 function getCreateLogPage(){
-    btn.innerHTML = `<button onclick="loginUser();" id="userLogButton">Log in</button>`;
+    btn.innerHTML = `<button onclick="loginUser();" id="userLogButton">Log In</button>`;
     title.innerHTML = `Log In`
     promt.innerHTML = `<span id="promt">Do not have an account?<button onclick="getCreateAccPage()" id="createAcc">Sign Up</button></span>`;
+    document.getElementById("user").value = "";
+    document.getElementById("password").value = "";
 }
