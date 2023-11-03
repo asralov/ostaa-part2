6acfdef1712f8eb984c5687bd24a011acf720464
pulyau <83565561+pulyau@users.noreply.github.com>
2023-11-03 12:36:10 -0700

    added comments to addUser

--- public_html/index.js
+++ public_html/index.js
@@ -29,16 +29,24 @@ function loginUser() {
   let pw = document.getElementById('password').value;
   console.log(us, pw);
 }
+/**
+ * This function adds a user to the database. If the username already exists
+ * the function does not save to the database and notifies the user that the 
+ * username has been taken
+ */
 function addUser() {
   let us = document.getElementById('user').value;
   let pw = document.getElementById('password').value;
   console.log(us, pw);
+  // Encodes the password for protection
   let p = fetch('/add/user/'+us+"/"+encodeURIComponent(pw));
   p.then((response) => {
     return response.text();
   }).then((text) => { 
+    // Shows the user the status message
     document.getElementById("createMessage").innerText = text;
-    if (text == "USER ALREADY EXISTS") {
+    // message element changes color depending on the message
+    if (text == "USER ALREADY EXISTS" || text == "USER SAVE ERROR") {
       document.getElementById("createMessage").style.color = "Red";
     } else if (text == "USER SUCCESSFULLY SAVED") {
       document.getElementById("createMessage").style.color = "Green";
